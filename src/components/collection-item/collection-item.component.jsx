139f2b5a59dff702f92578a61077cdffddfadb06
skelleher2 <seankelleher2@gmail.com>
2021-02-19 21:13:28 -0600

    creating signin page and sign in components, form-input, custom-button

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import './collection-item.styles.scss';
 
+
+
 const CollectionItem = ({ id, name, price, imageUrl }) => (
     <div className='collection-item'>
         <div className='image'
@@ -10,11 +12,11 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
         }}
         />
         <div className='collection-footer'>
-            <span className='name'>{ name }</span>
-            <span className='price'>{ price }</span>
+            <span className='name'>{name}</span>
+            <span className='price'>{price}</span>
         
         </div>
     </div>
-)
+);
 
 export default CollectionItem;
